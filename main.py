d33711322f809606daf4e23ba90f7a135cdde950
wincom42 <wincom42@gmail.com>
2022-07-02 15:16:44 +0530

    push file in github

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {'data':{'name':"parul singh"}}
+
+@app.get("/about")
+def about():
+     return {'data':{'name':"parul singsdfsdfh"}}
+
